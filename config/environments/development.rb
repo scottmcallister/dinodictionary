f31320964033c15c752fefad1b18434e53876ffb
Scott McAllister <do.not.tell.scotty@gmail.com>
2015-04-22 20:49:27 -0400

    switched to modal windows

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Prevent assets from loading twice
+  config.serve_static_files = false
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
