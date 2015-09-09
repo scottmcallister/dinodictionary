4e50531e759ce98f9187f69a33be8ed21adf7313
Scott McAllister <scottm@nextopia.com>
2015-09-08 22:03:47 -0400

    changed model search logic

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,10 +1,12 @@
 class Dinosaur < ActiveRecord::Base
 
 	def self.search(search, era, diet, location, taxinomic_order)
+		
 		if search
 			where("name LIKE ? AND era LIKE ? AND diet LIKE ? AND location LIKE ? AND taxonomic_order LIKE ?", "%#{search}%", "%#{era}%", "%#{diet}%", "%#{location}%", "%#{taxinomic_order}%")
 		else 
 			all
 		end
+		
 	end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -5,6 +5,9 @@ class Search < ActiveRecord::Base
 		dinosaurs = Dinosaur.all
 
 		dinosaurs = dinosaurs.where(["name LIKE ?", "%#{keywords}%"]) if keywords.present?
+		dinosaurs = dinosaurs.where(["era LIKE ?", "%#{era}%"]) if era.present?
+		dinosaurs = dinosaurs.where(["diet LIKE ?", "%#{diet}%"]) if diet.present?
+		dinosaurs = dinosaurs.where(["location LIKE ?", "%#{location}%"]) if location.present?
 		dinosaurs = dinosaurs.where(["taxonomic_order LIKE ?", "%#{taxonomic_order}%"]) if taxonomic_order.present?
 
 		return dinosaurs
